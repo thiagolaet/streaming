5d8c4c43400db36cbcd0d047b8f73189ec81dac2
Thiago Laet <thi4golaet@gmail.com>
2021-12-20 19:57:42 -0300

    create insert video form validation

--- streaming_server/frames/insert_video_frame.py
+++ streaming_server/frames/insert_video_frame.py
@@ -3,6 +3,8 @@ from tkinter import filedialog, Label, Button, Entry
 from repositories.video_repository import VideoRepository
 from services.video_file_manager_service import VideoFileManagerService
 
+EMPTY_VIDEO_LABEL = 'Nenhum vídeo selecionado'
+
 class InsertVideoFrame:
 
     def __init__(self, master):
@@ -24,23 +26,37 @@ class InsertVideoFrame:
         name_entry.grid(column=1, row=2, **self.padding)
         name_entry.focus()
         cancel_button = Button(self.frame, text="Cancelar", width=14, height=1, command=self.close)
-        cancel_button.grid(column=0, row=3, **self.padding)
+        cancel_button.grid(column=0, row=4, **self.padding)
         confirm_button = Button(self.frame, text="Confirmar", width=14, height=1, command=self.submit)
-        confirm_button.grid(column=1, row=3, **self.padding)
+        confirm_button.grid(column=1, row=4, **self.padding)
         self.frame.grid(**self.padding)
+        self.error_label = Label(self.frame, text='Erro', fg='red')
 
     def close(self):
         self.master.destroy()
 
     def select_videos(self):
-         self.video_path = filedialog.askopenfilename(initialdir="/", title="Selecione os vídeos")
+         self.video_path = filedialog.askopenfilename(initialdir="/", title=EMPTY_VIDEO_LABEL)
          self.frame.focus()
          self.video_name_output_label.config(text=self.video_path.split('/')[-1])
 
     def submit(self):
+        if not self.validate_form():
+            self.error_label.grid(column=0, row=3, columnspan=2)
+            return
         video_name = self.name_var.get()
         video_format = self.video_path.split('.')[-1]
         video_id = VideoRepository.insert(video_name, video_format)
         filename = f"{video_id}_{video_name.replace(' ', '_')}.{video_format}".lower()
         VideoFileManagerService.copy_file(self.video_path, filename)
         self.close()
+
+    def validate_form(self):
+        is_invalid = False
+        if self.video_path == EMPTY_VIDEO_LABEL or self.video_path == '':
+            self.error_label.config(text='Você deve selecionar um vídeo')
+            return False
+        if self.name_var.get() == '':
+            self.error_label.config(text='Você deve informar um nome para o vídeo')
+            return False
+        return True
